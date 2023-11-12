8887ca7d94385196c894a522436a5fd1dd9d2e79
busradertli <busradertli000@gmail.com>
2023-11-12 22:25:50 +0300

    Create variables.java

---
+++ Java101/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+	
+	public static void main(String [] args){
+		int ogrenciSayisi = 10;
+		double sayi = 12.5;
+		sayi = -129;
+		String mesaj = "Ögrenci sayım:";
+		char karakter = 'B';
+		boolean dogruMu = false;
+		
+		
+		System.out.println("Ögrenci sayım:" + ogrenciSayisi);
+		
+	}
+	
+}
