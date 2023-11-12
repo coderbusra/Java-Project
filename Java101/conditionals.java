f6b36e8c4f62a701e5339b5fe467af8a36b40ac9
busradertli <busradertli000@gmail.com>
2023-11-12 22:25:57 +0300

    Create conditionals.java

---
+++ Java101/conditionals.java
@@ -0,0 +1,16 @@
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		int sayi = 24;
+		if(sayi < 20) {
+			System.out.println("Sayi 20'den küçüktür.");	
+		}else if(sayi==20) {
+			System.out.println("Sayi 20'ye esittir");
+		}else {
+			System.out.println("Sayi 20'den büyüktür");
+		}
+		
+	}
+
+}
